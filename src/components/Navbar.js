ed27a01f616080f48db30429d65b5cbab52d85e6
Mustafa Haidari <mhaidarpoor@gmail.com>
2023-02-02 02:30:00 +1000

    Update logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,14 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[100px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+        <img
+          src={Logo}
+          alt="Logo Image"
+          style={{ width: "70px" }}
+          className="mt-4 ml-4"
+        />
       </div>
       {/* menu */}
       <ul className="hidden md:flex">
